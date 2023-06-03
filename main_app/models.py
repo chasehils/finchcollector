84a867dd51b18a9b81be808db3f5379810f9dca7
usernamechasehils <chasehilsinger@gmail.com>
2023-06-03 18:53:43 -0400

    update/delete

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
+
+
+MEALS = (
+        ('B', 'Breakfast'),
+        ('L', 'Lunch'),
+        ('D', 'Dinner')
+    )
 
 # Create your models here.
 class Finch(models.Model):
@@ -13,3 +21,20 @@ class Finch(models.Model):
     # no makemigrations are necessary
     def __str__(self):
         return f'{self.name} ({self.id})' 
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+    
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+        )
+    
+    def __str__(self):
+        # nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
